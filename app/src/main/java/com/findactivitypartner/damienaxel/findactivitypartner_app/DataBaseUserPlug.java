451700c8aca1c00cb14628197307457c55d4919c
axel-thiel <taxelt@gmail.com>
2016-08-04 10:27:14 +0200

    create DataBaseUserPlug class change UserHomeActivity background import pic of Damien, Mélanie and Axel

---
+++ app/src/main/java/com/findactivitypartner/damienaxel/findactivitypartner_app/DataBaseUserPlug.java
@@ -0,0 +1,60 @@
+package com.findactivitypartner.damienaxel.findactivitypartner_app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by maax on 04/08/16.
+ */
+public class DataBaseUserPlug {
+    public static List<FicheActivity> createDataBaseUserPlug(){
+
+        // bréation d'une base de donné avec 3 utilisateurs
+
+        FicheActivity fiche1 = new FicheActivity("Footing", "Lyon", "Damien", R.drawable.tintin
+                ,"Dimanche", "Mardi", false, false, "commentaire", "Niveau1");
+        FicheActivity fiche2 = new FicheActivity("Footing", "Bordeau", "Damien", R.drawable.tintin
+                ,"jeudi", "Mardi", true, false, "commentaire 2", "Niveau1");
+        FicheActivity fiche3 = new FicheActivity("Foot", "Lyon", "Damien", R.drawable.tintin
+                ,"mercredi", "Mardi", true, false, "commentaire 3", "Niveau1");
+        FicheActivity fiche4 = new FicheActivity("Footing", "Toulouse", "Damien", R.drawable.tintin
+                ,"lundi ", "lundi", false, false, "très aimable", "Niveau1");
+
+        FicheActivity fiche5 = new FicheActivity("Footing", "Lyon", "Axel", R.drawable.tintin
+                ,"Dimanche", "Mardi", false, false, "commentaire", "Niveau1");
+        FicheActivity fiche6 = new FicheActivity("Footing", "Troyes", "Axel", R.drawable.tintin
+                ,"jeudi", "Mardi", true, false, "commentaire 2", "Niveau1");
+        FicheActivity fiche7 = new FicheActivity("Foot", "Lyon", "Axel", R.drawable.tintin
+                ,"mercredi", "Mardi", true, false, "commentaire 3", "Niveau1");
+        FicheActivity fiche8 = new FicheActivity("Judo", "Toulouse", "Axel", R.drawable.tintin
+                ,"lundi ", "lundi", false, false, "très aimable", "Niveau1");
+
+        FicheActivity fiche9 = new FicheActivity("Footing", "Lyon", "Mélanie", R.drawable.tintin
+                ,"Dimanche", "Mardi", false, false, "commentaire", "Niveau1");
+        FicheActivity fiche10 = new FicheActivity("Footing", "Dijon", "Mélanie", R.drawable.tintin
+                ,"jeudi", "Mardi", true, false, "commentaire 2", "Niveau1");
+        FicheActivity fiche11 = new FicheActivity("Foot", "Lyon", "Mélanie", R.drawable.tintin
+                ,"mercredi", "Mardi", true, false, "commentaire 3", "Niveau1");
+        FicheActivity fiche12 = new FicheActivity("Judo", "Toulouse", "Mélanie", R.drawable.tintin
+                ,"lundi ", "lundi", false, false, "très aimable", "Niveau1");
+
+        List<FicheActivity> listeFiches = new ArrayList<>();
+        listeFiches.add(fiche1);
+        listeFiches.add(fiche2);
+        listeFiches.add(fiche3);
+        listeFiches.add(fiche4);
+        listeFiches.add(fiche5);
+        listeFiches.add(fiche6);
+        listeFiches.add(fiche7);
+        listeFiches.add(fiche8);
+        listeFiches.add(fiche9);
+        listeFiches.add(fiche10);
+        listeFiches.add(fiche11);
+        listeFiches.add(fiche12);
+
+
+        return listeFiches;
+    }
+
+
+}
--- app/src/main/java/com/findactivitypartner/damienaxel/findactivitypartner_app/userHomeActivity.java
+++ app/src/main/java/com/findactivitypartner/damienaxel/findactivitypartner_app/userHomeActivity.java
@@ -1,7 +1,6 @@
 package com.findactivitypartner.damienaxel.findactivitypartner_app;
 
 import android.app.Activity;
-import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -24,21 +23,7 @@ public class userHomeActivity extends Activity {
         TextView userNameTextView = (TextView) findViewById(R.id.userNameTextView);
         userNameTextView.setText("Tintin Milou");
 
-        FicheActivity fiche1 = new FicheActivity("Ponney aquatique", "troyes", "Tintin", R.drawable.tintin
-                                                ,"Lundi", "Mardi", false, false, "commentaire 1", "Niveau1");
-        FicheActivity fiche2 = new FicheActivity("Babyfoot", "Bordeau", "Tintin", R.drawable.tintin
-                                                ,"jeudi", "Mardi", true, false, "commentaire 2", "Niveau1");
-        FicheActivity fiche3 = new FicheActivity("pétanque", "Paris", "Tintin", R.drawable.tintin
-                                                ,"mercredi", "Mardi", true, false, "commentaire 3", "Niveau1");
-        FicheActivity fiche4 = new FicheActivity("FF", "Lyon", "kikim", R.drawable.kikim
-                                                ,"lundi ", "lundi", false, false, "très aimable", "Niveau1");
-
-        List<FicheActivity> listeFiches = new ArrayList<>();
-        listeFiches.add(fiche1);
-        listeFiches.add(fiche2);
-        listeFiches.add(fiche3);
-        listeFiches.add(fiche4);
-
+        List<FicheActivity> listeFiches = DataBaseUserPlug.createDataBaseUserPlug();
 
         final ListView listeUserFiches = (ListView) findViewById(R.id.liste_fiches_user);
         FicheUserAdapter ficheUserAdapter= new FicheUserAdapter(this, R.layout.adapter_fiche_user,listeFiches);
