587fc13f434d49d115507af6b47b5ebb3a6f3b38
dams156 <damiengivelet@gmail.com>
2016-09-09 11:08:28 +0200

    spinner on city and level

--- app/src/main/java/com/findactivitypartner/damienaxel/findactivitypartner_app/CreateNewCardActivity.java
+++ app/src/main/java/com/findactivitypartner/damienaxel/findactivitypartner_app/CreateNewCardActivity.java
@@ -24,38 +24,48 @@ public class CreateNewCardActivity extends Activity {
     private TextView textViewCity;
     private TextView textViewLevel;
     private TextView textViewComment;
-    private Spinner spinnerActivity;
+    private Spinner spinnerActivity,spinnerCity,spinnerLevel;
+    private String userMail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Bundle bundle = getIntent().getExtras();
         userLogin = bundle.getString("userLoginString");
-
+        userMail = bundle.getString("userMailString");
         setContentView(R.layout.create_card_user);
         textViewLogin = (TextView) findViewById(R.id.new_card_login);
         textViewMail = (TextView) findViewById(R.id.new_card_email);
-        textViewActivity = (TextView) findViewById(R.id.new_card_activity);
-        textViewCity = (TextView) findViewById(R.id.new_card_city);
-        textViewLevel = (TextView) findViewById(R.id.new_card_level);
+        //textViewActivity = (TextView) findViewById(R.id.new_card_activity);
+        //textViewCity = (TextView) findViewById(R.id.new_card_city);
+        //textViewLevel = (TextView) findViewById(R.id.new_card_level);
         textViewComment = (TextView) findViewById(R.id.new_card_comment);
         spinnerActivity = (Spinner) findViewById(R.id.spinner_activity);
+        spinnerCity = (Spinner) findViewById(R.id.spinner_city);
         textViewLogin.setText(userLogin);
+        ArrayAdapter<CharSequence> adaptercity = ArrayAdapter.createFromResource(this,
+                R.array.city_array, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.activity_array, android.R.layout.simple_spinner_item);
     // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adaptercity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     // Apply the adapter to the spinner
         spinnerActivity.setAdapter(adapter);
+        spinnerCity.setAdapter(adaptercity);
+        spinnerLevel = (Spinner) findViewById(R.id.spinner_level);
+        ArrayAdapter<CharSequence> adapterlevel = ArrayAdapter.createFromResource(this,
+                R.array.level_array, android.R.layout.simple_spinner_item);
         textViewMail.setText(userMail);
-
+        adapterlevel.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLevel.setAdapter(adapterlevel);
         }
     public void onCreateResearchCard(View view) {
 
-        Card newCard = new Card(textViewActivity.getText().toString(),
-                textViewCity.getText().toString(),
+        Card newCard = new Card(spinnerActivity.getSelectedItem().toString(),
+                spinnerCity.getSelectedItem().toString(),
                 userLogin,
-                textViewLevel.getText().toString(),
+                spinnerLevel.getSelectedItem().toString(),
                 userMail,
                 textViewComment.getText().toString());
 
